bae36514bb7d772d180e7cfca4388d06705cd6ec
Markuss <jjansonsmarkuss@gmail.com>
2025-12-03 16:02:39 +0200

    Menu screen almost done

---
+++ Assets/Scripts/DiceAnimationScript.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class DiceAnimationScript : MonoBehaviour
+{
+
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void RollDice()
+    {
+        animator.SetBool("isRolling", true);
+    }
+
+
+    public void StopDice()
+    {
+        animator.SetBool("isRolling", false);
+    }
+}
+

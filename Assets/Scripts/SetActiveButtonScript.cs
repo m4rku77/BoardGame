fade59e0d029efb08ef0b7bf3ca7b5d963876536
Markuss <jjansonsmarkuss@gmail.com>
2025-12-04 08:58:22 +0200

    Menu screen and buttons work

---
+++ Assets/Scripts/SetActiveButtonScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using UnityEngine;
+
+public class SetActiveButtonScript : MonoBehaviour
+{
+    public GameObject targetObject;
+
+    public void ToggleActiveAfterDeleay(float delay)
+    {
+        StartCoroutine(ToggleActiveCoroutine(delay));
+    }
+
+    private IEnumerator ToggleActiveCoroutine(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        targetObject.SetActive(!targetObject.activeSelf);
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+
+
+}
